3a02eb30e13d1ed2a9c712bb927907ab46bb8273
Brian <bmcb555@gmail.com>
2021-10-14 08:53:20 +0100

    Getting Started - First EC2 instance

---
+++ Getting Started/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "3.62.0"
+    }
+  }
+}
+
+provider "aws" {
+  profile = "default"
+  region  = "us-east-2"
+}
+
+resource "aws_instance" "test-instance" {
+  ami           = "ami-074cce78125f09d61"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Test Instance for Training"
+  }
+}
